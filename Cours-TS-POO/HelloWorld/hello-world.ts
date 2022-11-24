a77df29ea1b28f07d7b9cf12937df4b7c714358a
Jean-Jacques Goddet <jj.goddet@icloud.com>
2022-11-24 11:29:51 +0100

    cours typescrypt

---
+++ Cours-TS-POO/HelloWorld/hello-world.ts
@@ -0,0 +1,13 @@
+/*const helloworld: string = "Hello World";
+const prenom: string = "Jean-Jacques";
+const age: number = 43;
+console.log("Coucou " + prenom + "<3");
+console.log(`Coucou ${prenom} <3`);*/
+
+/**
+ * cette fonction permet de dire bonjour !
+ */
+function helloWorld(): string {
+    //console.log("Hello World !");
+    return "Hello World !"
+}
