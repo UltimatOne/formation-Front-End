90af9dc084d351557308ed068fc4331fecda934e
Jean-Jacques Goddet <jj.goddet@icloud.com>
2022-11-28 11:03:43 +0100

    feat: exercice10

---
+++ Cours-TS-POO/exercice10/exercice10.js
@@ -0,0 +1,17 @@
+/**
+ * retourne la température plus proche de zero
+ * @param  {...any} temperatures liste de températures
+ * @returns température
+ */
+
+
+function temperatureProcheDeZero(...temperatures: number[]): number{
+    let plusProcheDeZero = temperatures[0];
+    for (let temperature of temperatures) {
+        if (Math.abs(temperature) < Math.abs(plusProcheDeZero)) {
+            plusProcheDeZero = temperature
+        }
+    }
+    return plusProcheDeZero;
+};
+console.log(temperaturePlusProcheDeZero(11, 82, 6, -8, -9, -8, -9, 18, -30))
