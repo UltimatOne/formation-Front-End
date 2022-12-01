f6aa517de14bd3cbaf05234f51a9c036183e5587
Jean-Jacques Goddet <jj.goddet@icloud.com>
2022-12-01 13:41:39 +0100

    mod: guerrier ts

--- Cours-TS-POO/PoneyFighter/Interfaces/Guerrier.ts
+++ Cours-TS-POO/PoneyFighter/Interfaces/Guerrier.ts
@@ -1,9 +1,10 @@
-export default interface Guerrier{
-    energie: number
+export default interface Guerrier {
+    energie: number;
 
     /**
-     * Lance une attaque puissante qui fait mal
-     * @param nombreDePm
+     * Lance une attaque physique qui fait des dégâts !
+     * La puissance de l'attaque est proportionnelle à l'énergie utilisée.
+     * @param nombreDenergie Nombre d'énergie à utiliser pour l'attaque
      */
-    attaquePuissante(nombreDeEnergie: number): number
+    attaquePuissante(nombreEnergie: number): number;
 }
