d2885f8a1931136e11e7f338a8948d808f30cb0b
Jean-Jacques Goddet <jj.goddet@icloud.com>
2022-12-05 09:48:34 +0100

    ajout cv, pitch et commandes git

--- Cours-TS-POO/src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import bonjour from ".";
-
-describe("bonjour", () => {
-    it("should return 'Bonjour'", () => {
-        expect(bonjour()).toBe("Bonjour");
-    });
-});
--- Cours-TS-POO/src/index.ts
+++
@@ -1,3 +0,0 @@
-export default function bonjour(): string{
-    return "Bonjour";
-}
