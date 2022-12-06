b0a1bbb2a0544174592f9f55c71e289cd4e7f43f
Jean-Jacques Goddet <jj.goddet@icloud.com>
2022-12-06 15:58:21 +0100

    react contacts

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,93 +1,45 @@
+import "./app.css";
+import Contact from "./components/contacts/contact";
+import Bonjour from "./components/bonjour/bonjour"
 function App() {
-  return (
-  <>
-    <div>
-        <div>
-          <h3>Contact</h3>
-        </div>
-
-        <div>
-          <h3>1</h3>
-        </div>
-
-        <div>
-          <h4>mail1@mail.com<br/>0600000001</h4>
-        </div>
-
-        <div>
-          <button>Supprimer</button>
-        </div>
-    </div>
-    <div>
-    <div>
-          <h3>Contact</h3>
-        </div>
-
-        <div>
-          <h3>2</h3>
-        </div>
-
-        <div>
-          <h4>mail2@mail.com<br/>0600000002</h4>
-        </div>
-
-        <div>
-          <button>Supprimer</button>
-        </div>
-    </div>
-    <div>
-    <div>
-          <h3>Contact</h3>
-        </div>
-
-        <div>
-          <h3>3</h3>
-        </div>
-
-        <div>
-          <h4>mail3@mail.com<br/>0600000003</h4>
-        </div>
-
-        <div>
-          <button>Supprimer</button>
-        </div>
-    </div>
-    <div>
-    <div>
-          <h3>Contact</h3>
-        </div>
-
-        <div>
-          <h3>4</h3>
-        </div>
-
-        <div>
-          <h4>mail4@mail.com<br/>0600000004</h4>
-        </div>
-
-        <div>
-          <button>Supprimer</button>
-        </div>
-    </div>
-    <div>
-    <div>
-          <h3>Contact</h3>
-        </div>
-
-        <div>
-          <h3>5</h3>
-        </div>
-
-        <div>
-          <h4>mail5@mail.com<br/>0600000005</h4>
-        </div>
-
-        <div>
-          <button>Supprimer</button>
-        </div>
-    </div>
-  </> 
-
+  const paragraphe = true;
+  const personnes = [{
+    nom: "Contact",
+    prenom: "1",
+    mail: "contact1@aol.com",
+    Tel: "0600000001",
+  },
+  {
+    nom: "Contact",
+    prenom: "1",
+    mail: "contact1@aol.com",
+    Tel: "0600000001",
+  },
+  {
+    nom: "Contact",
+    prenom: "1",
+    mail: "contact1@aol.com",
+    Tel: "0600000001",
+  },
+  {
+    nom: "Contact",
+    prenom: "1",
+    mail: "contact1@aol.com",
+    Tel: "0600000001",
+  } ,
+  {
+    nom: "Contact",
+    prenom: "1",
+    mail: "contact1@aol.com",
+    Tel: "0600000001",
+  }];
+return (
+<>
+  <Bonjour/>
+  <h1 className= {paragraphe ? "para" : "dauphin"}>Mes Contacts</h1>
+  {paragraphe ? <p>Je suis un dauphin</p> : <p>Jes suis une licorne</p>}
+  {personnes.map((personne) => (<Contact {...personne}/>))} 
+</>
   );
 }
 
---
+++ my-app/src/components/bonjour/bonjour.jsx
@@ -0,0 +1,4 @@
+function Bonjour(props){
+    return <h2>Bonjour {props.prenom}</h2>
+}
+export default Bonjour
