019cbf38803612e1c5ff82ebe486335dcb65beed
Jean-Jacques Goddet <jj.goddet@icloud.com>
2022-12-06 11:33:14 +0100

    ajout exercice react

---
+++ my-app/src/App.js
@@ -0,0 +1,94 @@
+function App() {
+  return (
+  <>
+    <div>
+        <div>
+          <h3>Contact</h3>
+        </div>
+
+        <div>
+          <h3>1</h3>
+        </div>
+
+        <div>
+          <h4>mail1@mail.com<br/>0600000001</h4>
+        </div>
+
+        <div>
+          <button>Supprimer</button>
+        </div>
+    </div>
+    <div>
+    <div>
+          <h3>Contact</h3>
+        </div>
+
+        <div>
+          <h3>2</h3>
+        </div>
+
+        <div>
+          <h4>mail2@mail.com<br/>0600000002</h4>
+        </div>
+
+        <div>
+          <button>Supprimer</button>
+        </div>
+    </div>
+    <div>
+    <div>
+          <h3>Contact</h3>
+        </div>
+
+        <div>
+          <h3>3</h3>
+        </div>
+
+        <div>
+          <h4>mail3@mail.com<br/>0600000003</h4>
+        </div>
+
+        <div>
+          <button>Supprimer</button>
+        </div>
+    </div>
+    <div>
+    <div>
+          <h3>Contact</h3>
+        </div>
+
+        <div>
+          <h3>4</h3>
+        </div>
+
+        <div>
+          <h4>mail4@mail.com<br/>0600000004</h4>
+        </div>
+
+        <div>
+          <button>Supprimer</button>
+        </div>
+    </div>
+    <div>
+    <div>
+          <h3>Contact</h3>
+        </div>
+
+        <div>
+          <h3>5</h3>
+        </div>
+
+        <div>
+          <h4>mail5@mail.com<br/>0600000005</h4>
+        </div>
+
+        <div>
+          <button>Supprimer</button>
+        </div>
+    </div>
+  </> 
+
+  );
+}
+
+export default App;
