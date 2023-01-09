7b9646edfa5bab24db70f60091933c0ba0de4161
Jean-Jacques Goddet <jj.goddet@icloud.com>
2023-01-09 09:16:54 +0100

    début cours Angular

--- React/TP/netfloux/src/composants/navbar/navbar.jsx
+++
@@ -1,8 +0,0 @@
-import logo from "./logo-netfloox.png";
-import "./navbar.css";
-export default function Navbar(){
-   return (
-    <div id="Navbar">
-        <img src={logo} alt="Logo Netfloux"/>
-    </div>
-)};
