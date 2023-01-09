0bcb9b08e7ed98165e31fcfb9ae3084cbc344c4f
Jean-Jacques Goddet <jj.goddet@icloud.com>
2023-01-09 10:26:04 +0100

    ajout composant bouton dans app

---
+++ Angular/testAngular/src/app/component/bouton/bouton.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bouton',
+  templateUrl: './bouton.component.html',
+  styleUrls: ['./bouton.component.scss']
+})
+export class BoutonComponent {
+
+}
