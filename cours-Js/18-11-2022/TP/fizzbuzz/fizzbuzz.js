d14f630cfad998b5aa34613880b745bb81523e5e
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2023-05-25 12:45:16 +0200

    angular

--- cours-Js/18-11-2022/TP/fizzbuzz/fizzbuzz.js
+++ cours-Js/18-11-2022/TP/fizzbuzz/fizzbuzz.js
@@ -10,3 +10,10 @@ for (let i = 1; i < 16; i++) {
     else {
         document.getElementById('root').innerHTML += '<br>' + i + '<br>'}
 }
+
+for (let i = 1; i < 16; i++) {
+    if (i % 15 == 0) console.log("FizzBuzz");
+    else if (i % 3 == 0) console.log("Fizz");
+    else if (i % 5 == 0) console.log("Buzz");
+    else console.log(i);
+}
