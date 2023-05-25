9aa7e61a7d85ff3909bbcbe4204d7f271924a01e
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2023-05-25 12:57:26 +0200

    fizzbuzz en affichage sur fizzbuzz.html

--- cours-Js/18-11-2022/TP/fizzbuzz/fizzbuzz.js
+++ cours-Js/18-11-2022/TP/fizzbuzz/fizzbuzz.js
@@ -1,19 +1,18 @@
-
-
 for (let i = 1; i < 16; i++) {
-    if (i % 15 == 0){
-        document.getElementById('root').innerHTML += '<br>' + "FizzBuzz" + '<br>'}
-    else if (i % 3 == 0) {
-        document.getElementById('root').innerHTML += '<br>' + "Fizz" + '<br>'}
-    else if (i % 5 == 0) {
-        document.getElementById('root').innerHTML += '<br>' + "Buzz" + '<br>'}
-    else {
-        document.getElementById('root').innerHTML += '<br>' + i + '<br>'}
+  if (i % 15 == 0) {
+    document.getElementById("root").innerHTML += "<br>" + "FizzBuzz" + "<br>";
+  } else if (i % 3 == 0) {
+    document.getElementById("root").innerHTML += "<br>" + "Fizz" + "<br>";
+  } else if (i % 5 == 0) {
+    document.getElementById("root").innerHTML += "<br>" + "Buzz" + "<br>";
+  } else {
+    document.getElementById("root").innerHTML += "<br>" + i + "<br>";
+  }
 }
 
 for (let i = 1; i < 16; i++) {
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
+  if (i % 15 == 0) console.log("FizzBuzz");
+  else if (i % 3 == 0) console.log("Fizz");
+  else if (i % 5 == 0) console.log("Buzz");
+  else console.log(i);
 }
