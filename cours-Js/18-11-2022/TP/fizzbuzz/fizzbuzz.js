17cb991d13c602d5d45c3d627fae25f75b285c77
UltimatOne <116150495+UltimatOne@users.noreply.github.com>
2023-05-26 08:43:33 +0200

    remplacement de innerHTML par querySelector et append

--- cours-Js/18-11-2022/TP/fizzbuzz/fizzbuzz.js
+++ cours-Js/18-11-2022/TP/fizzbuzz/fizzbuzz.js
@@ -4,15 +4,16 @@ root.append(contenu);
 
 for (let i = 1; i < 16; i++) {
   if (i % 15 == 0) {
-    contenu += "FizzBuzz" + "\n";
+    contenu += "FizzBuzz\n";
   } else if (i % 5 == 0) {
-    contenu += "Buzz" + "\n";
+    contenu += "Buzz\n";
   } else if (i % 3 == 0) {
-    contenu += "Fizz" + "\n";
+    contenu += "Fizz\n";
   } else {
     contenu += i + "\n";
   }
 }
+root.append(contenu);
 
 for (let i = 1; i < 16; i++) {
   if (i % 15 == 0) console.log("FizzBuzz");
